101fe98d5b42c2e2b8d1efc8ae6acf1c4372f85b
YashRevdiwala <yash.yr.2493@gmail.com>
2024-06-24 16:11:07 +0530

    initial commit

---
+++ components/about.tsx
@@ -0,0 +1,37 @@
+"use client"
+
+import React from "react"
+import SectionHeading from "./section-heading"
+import { motion } from "framer-motion"
+import { useSectionInView } from "@/lib/hooks"
+
+export default function About() {
+  const { ref } = useSectionInView("About")
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About me</SectionHeading>
+      <p className="mb-3">
+        I'm a skilled software developer with knowledge in HTML5, CSS, PHP,
+        JavaScript, MYSQL, React.js, Node.js and MongoDB. I'm a quick learner
+        and will collaborate closely with clients to create efficient, scalable,
+        and user-friendly solutions that solve real-world problems.
+      </p>
+
+      <p>
+        <span className="italic">When I'm not coding</span>, I enjoy playing
+        video games and watching movies. I also enjoy{" "}
+        <span className="font-medium">learning new things</span>. I am currently
+        learning about{" "}
+        <span className="font-medium">history and philosophy</span>.
+      </p>
+    </motion.section>
+  )
+}
